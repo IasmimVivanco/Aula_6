c4ae978a2979a879523fdb00b2596e50e67daeaa
IasmimVivanco <iasmim.vivanco@hotmail.com>
2022-06-08 22:49:29 -0300

    Adicionei o código fonte do desafio 1

---
+++ Código fonte desafio 1/Código fonte desafio 1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite seu nome:");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Digite sua idade: ");
+int idade = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o ano atual:");
+int ano = int.Parse(Console.ReadLine());
+
+int anoDoNascimento = ano - idade;
+
+Console.WriteLine($"Olá, {nome} você nasceu em {anoDoNascimento}");
